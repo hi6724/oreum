19b6c9f3fe4c6f9d4ceb8ded88c08cd8f80c1ba6
hi6724 <hi6724@gmail.com>
2024-05-23 21:11:40 +0900

    ✨ theme 색 추가

--- src/components/ThemeClient.tsx
+++ src/components/ThemeClient.tsx
@@ -3,13 +3,27 @@
 import { ThemeProvider, createGlobalStyle } from "styled-components";
 
 export const lightTheme = {
-  green: "#34582E",
-  stone: "#53504F",
-  orange: "#EB7E30",
-  yellow: "#FDCA68",
-  gray: "#D9D9D9",
-  black: "#292929",
-  white: "#F1F1F1",
+  white: "#fff",
+  gray01: "#e7e7e7",
+  gray02: "#dcdcdc",
+  gray03: "#c7c7c7",
+  gray04: "#8b8b8b",
+  gray05: "#646464",
+  gray06: "#292929",
+  gray07: "#1d1d1d",
+  black: "#000",
+  green01: "#90a68d",
+  green02: "#80a77a",
+  green03: "#5d8058",
+  green04: "#34582e",
+  bg: "#f1f1f1",
+  beige01: "#eae6e0",
+  beige02: "#dbd6ce",
+  beige03: "#c7bbab",
+  pink: "#d67c7c",
+  yello: "#e7c177",
+  orange: "#e59256",
+  blue: "#a1cad7",
 };
 
 export const GlobalStyle = createGlobalStyle`
